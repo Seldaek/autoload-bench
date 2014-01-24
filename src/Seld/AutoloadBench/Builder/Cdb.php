c90c056887297244cc0cfddbf77952084ca09955
Marcus Winkler <marcus.winkler@gmx.de>
2014-01-24 19:38:26 +0000

    Add cdb (constant database) loader

---
+++ src/Seld/AutoloadBench/Builder/Cdb.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Seld\AutoloadBench\Builder;
+
+use Seld\AutoloadBench\Builder;
+
+class Cdb extends Builder
+{
+    protected function build($classes, $path)
+    {
+        $dbfile = $this->path . '/loader.cdb';
+
+        $cdb = dba_open($dbfile, 'n', 'cdb');
+
+        foreach ($classes as $class) {
+            dba_insert($class, $path.'/'.strtr($class, '\\', '/').'.php', $cdb);
+        }
+
+        dba_close($cdb);
+
+        $code = '<?php return new \Seld\AutoloadBench\Loader\Cdb(\'%s\');';
+
+        file_put_contents($this->path.'/loader.php', sprintf($code, $dbfile));
+    }
+}
+
